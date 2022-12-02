d637d2d2a7f6269a178d5e4221d13df0294c78a5
bomyuniverse <bomi@soongsil.ac.kr>
2022-12-02 10:48:11 +0900

    publish

---
+++ exampleCode/exampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleCodeApp.swift
+//  exampleCode
+//
+//  Created by 김보미 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct exampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
